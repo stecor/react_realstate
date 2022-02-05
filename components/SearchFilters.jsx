47da80b37f79caa2f86fe8b20fa3cf53cb210295
Stefano Corra <stefano.corra@hotmail.com>
2022-02-04 20:57:51 -0500

    search-filter

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,13 +8,48 @@ import {
   Spinner,
   Icon,
   Button,
+  flexbox,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>searchFilters</div>;
+  const router = useRouter();
+  const [filters] = useState(filterData);
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+    router.push({ pathname: path, query });
+  };
+
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w='fit-content'
+            p='2'
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
---
+++ pages/property/[id].js
@@ -0,0 +1,5 @@
+import { Box, Flex, Spacer, Text, Avatar } from "@chakra-ui/react";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";

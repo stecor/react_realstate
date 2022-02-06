67d4470429dce554ce3797513b1e9a0149f6c90f
Stefano Corra <stefano.corra@hotmail.com>
2022-02-05 22:35:38 -0500

    nprogress

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -23,7 +23,10 @@ const SearchFilters = () => {
     const { query } = router;
     const values = getFilterValues(filterValues);
     values.forEach((item) => {
-      query[item.name] = item.value;
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value;
+      }
+      
     });
     router.push({ pathname: path, query });
   };

13e1e4611b52437845acd39221cf50dd2b3d11ac
Stefano Corra <stefano.corra@hotmail.com>
2022-02-05 21:27:55 -0500

    details page

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -4,7 +4,7 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 import { baseUrl, fetchApi } from "../../utils/fetchApi";
-import ImageScrollbar from "../../utils/fetchApi";
+import ImageScrollbar from "../../components/ImageScrollbar";
 
 const propertyDetails = ({
   propertyDetails: {
@@ -26,6 +26,34 @@ const propertyDetails = ({
 }) => (
   <Box maxWidth="1000px" margin="auto" p="4">
     {photos && <ImageScrollbar data={photos} />}
+    <Box w="full" p="6">
+      <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Box paddingRight="3" color="green.400">
+            {isVerified && <GoVerified />}
+          </Box>
+          <Text fontWeight="bold" fontSize="lg">
+            $ {millify(price)}
+            {rentFrequency && `/${rentFrequency}`}
+          </Text>
+        </Flex>
+        <Box>
+          <Avatar size="sm" src={agency?.logo?.url} />
+        </Box>
+      </Flex>
+      <Flex
+        alignItems="center"
+        p="1"
+        justifyContent="space-between"
+        w="250px"
+        color="blue.400"
+      >
+        {rooms}
+        <FaBed />|{baths}
+        <FaBath />|{millify(area)} sqft <BsGridFill />
+      </Flex>
+      <Text fontSize="lg">{title}</Text>
+    </Box>
   </Box>
 );
 
